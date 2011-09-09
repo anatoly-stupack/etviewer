8d76e8faa9c9302c53caee3e47ee1b5ab6507713
jmartin_cp <etviewer@gmail.com>
2011-09-09 18:29:41 +0000

    Fixed "Log to file" accelerator.

--- trunk/ETViewer/resource.h
+++ trunk/ETViewer/resource.h
@@ -181,18 +181,18 @@
 #define ID_FILE_RECENTLOGS              32835
 #define ID_RECENTLOGS_                  32836
 #define ID_FILE_CLOSELOG                32837
-#define ID_FILE_LOGTOFILE               32838
 #define ID_FILE_STOPLOGGINTOFILE        32839
 #define ID_EDIT_SETTINGS                32840
 #define ID_PROVIDER_TREE_RELOAD_PROVIDER 32842
 #define ID_PROVIDER_TREE_RELOAD_ALL_PROVIDERS 32845
+#define ID_FILE_LOGTOFILE               32846
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        157
-#define _APS_NEXT_COMMAND_VALUE         32846
+#define _APS_NEXT_COMMAND_VALUE         32847
 #define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
