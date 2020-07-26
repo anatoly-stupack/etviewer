e5ed9a032090d65bf3695f0c59960cef99277249
Anatoly Stupack <anatoly.stupack@gmail.com>
2020-07-26 17:28:23 +0200

    Add package restore to build script

--- build/build.cmd
+++ build/build.cmd
@@ -2,4 +2,10 @@ echo off
 
 set ROOT=%~dp0\..\
 
+nuget restore %ROOT%trunk\ETViewer.sln
+IF NOT %ERRORLEVEL% == 0 goto end
+
 msbuild %ROOT%trunk\ETViewer.sln -p:Configuration=Release -p:Platform=Win32
+IF NOT %ERRORLEVEL% == 0 goto end
+
+:end
