f1581f4254e848857e729a77622a5897196f8a88
Anatoly Stupack <anatoly.stupack@gmail.com>
2020-08-08 17:25:33 +0200

    Rename trunk to src

--- build/build.cmd
+++ build/build.cmd
@@ -2,10 +2,10 @@ echo off
 
 set ROOT=%~dp0\..\
 
-nuget restore %ROOT%trunk\ETViewer.sln
+nuget restore %ROOT%src\ETViewer.sln
 IF NOT %ERRORLEVEL% == 0 goto end
 
-msbuild %ROOT%trunk\ETViewer.sln -p:Configuration=Release -p:Platform=Win32
+msbuild %ROOT%src\ETViewer.sln -p:Configuration=Release -p:Platform=Win32
 IF NOT %ERRORLEVEL% == 0 goto end
 
 :end
