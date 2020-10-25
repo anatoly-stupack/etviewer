aa6641a8a877d68bb62cb185553e290384319b73
Anatoly Stupack <anatoly.stupack@gmail.com>
2020-10-25 12:31:10 +0100

    Refresh wpp templates in TraceExample, add new checks

--- src/TraceExample/TraceExample.cpp
+++ src/TraceExample/TraceExample.cpp
@@ -1,8 +1,4 @@
-// TraceExample.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
 #include "WPPGuid.h"
 #include "TraceExample.tmh"
 
@@ -10,17 +6,23 @@ int _tmain()
 {
     WPP_INIT_TRACING( L"TraceExample" );
 
-    DoTraceMessage(TRACE_DEBUG, _T("Test wide string message %S"), _T("string"));
-    DoTraceMessage(TRACE_DEBUG, "Test narrow string message %s", "string");
+    DoTraceMessage(TRACE_DEBUG, L"=============================================");
+    DoTraceMessage(TRACE_DEBUG, L" Welcome to Etviewer Test application");
+    DoTraceMessage(TRACE_DEBUG, L"=============================================");
 
-    DoTraceMessage(TRACE_DEBUG, _T("This is a number %d"), 1);
+    DoTraceMessage(TRACE_DEBUG, L"Test int %d", 12345);
+    DoTraceMessage(TRACE_DEBUG, L"Test hex 0x%x", 0xABCD123);
 
-    //DoTraceMessage(TRACE_DEBUG, _T("Test special type IPADDR %!iPADDR!"), 1);
-    DoTraceMessage(TRACE_DEBUG, _T("Test special type PORT %!PORT!"), 1);
-    DoTraceMessage(TRACE_DEBUG, _T("Test special type STATUS %!STATUS!"), 1);
-    DoTraceMessage(TRACE_DEBUG, _T("Test special type WINERROR %!WINERROR!"), 1);
-    DoTraceMessage(TRACE_DEBUG, _T("Test special type HRESULT %!HRESULT!"), (HRESULT)1);
-    DoTraceMessage(TRACE_DEBUG, _T("Test special type NDIS_STATUS %!NDIS_STATUS!"), 1);
+    DoTraceMessage(TRACE_DEBUG, L"Test wide string message %ws", L"string");
+    DoTraceMessage(TRACE_DEBUG, L"Test narrow string message %s", "string");
+
+    DoTraceMessage(TRACE_DEBUG, L"This is a number %d", 1);
+
+    DoTraceMessage(TRACE_DEBUG, L"Test special type PORT %!PORT!", 1);
+    DoTraceMessage(TRACE_DEBUG, L"Test special type STATUS %!STATUS!", 1);
+    DoTraceMessage(TRACE_DEBUG, L"Test special type WINERROR %!WINERROR!", 1);
+    DoTraceMessage(TRACE_DEBUG, L"Test special type HRESULT %!HRESULT!", (HRESULT)1);
+    DoTraceMessage(TRACE_DEBUG, L"Test special type NDIS_STATUS %!NDIS_STATUS!", 1);
 
     WPP_CLEANUP();
     return 0;
