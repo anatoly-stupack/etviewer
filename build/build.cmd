0894787a5b22414b4a8c5107ad5a639b94cf1aa3
Anatoly Stupack <anatoly.stupack@gmail.com>
2020-11-01 14:44:49 +0100

    Remove build tools from repository

--- build/build.cmd
+++ build/build.cmd
@@ -2,9 +2,15 @@ echo off
 
 set ROOT=%~dp0\..\
 
+if exist nuget.exe goto restore
+
+powershell.exe -Command "Invoke-WebRequest -OutFile nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+:restore
 nuget restore %ROOT%src\ETViewer.sln
 IF NOT %ERRORLEVEL% == 0 goto end
 
+:build
 msbuild %ROOT%src\ETViewer.sln -p:Configuration=Release -p:Platform=Win32
 IF NOT %ERRORLEVEL% == 0 goto end
 
